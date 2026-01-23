5bdffa6921a5754d584de90962de50a5bf5d2580
Christiano Gonçalves <chrispeixe00@gmail.com>
2026-01-23 03:48:44 -0300

    feat: concluido o modulo e colocado resumo da aula

--- Mundo_2_py/12_aninhada.py
+++ Mundo_2_py/12_aninhada.py
@@ -1,14 +1,26 @@
-'''Condiçoes aninhadas''' #uma coisa dentro da outra, ninho
+'''
+CONDIÇÕES ANINHADAS (MUNDO 2)
+=============================
+RESUMO DA AULA: 
+- Estrutura: if (se), elif (senão se), else (senão).
+- Lógica: Escada de decisões onde apenas um bloco é executado.
+- Ferramentas: Operadores lógicos (and, or, in), Formatação (:.2f),
+  Bibliotecas (datetime para o ano atual, random para sorteios).
+- Engenharia: Foco em evitar repetição (DRY) e validar entradas do usuário.
+'''
+
 from datetime import date
+from random import randint
+from time import sleep
 
-
+# TEORIA EXEMPLO: PRÁTICA DE NOMES
 def pratica():
-    nome=str(input('Qual seu nome? '))
+    nome = str(input('Qual seu nome? ')).strip()
+    
     if nome == 'Christiano':
         print('Que nome bonito!')
-    elif nome =='Pedro' or nome == 'Maria' or nome=='Paulo':
+    elif nome in 'Pedro Maria Paulo':
         print('Seu nome é bem popular!') 
-        
     elif nome in 'Ana Cláudia Jéssica Juliana':
         print('Belo nome feminino.')
     else:
@@ -16,185 +28,116 @@ def pratica():
     print(f'Tenha um bom dia, {nome}!')
 
 
+
+# EXERCÍCIO 36: APROVANDO EMPRÉSTIMO
 def ex36():
-    valor=float(input('Qual valor da casa? R$ '))
-    sal=float(input('Qual salario do comprador? R$ '))
-    anos=int(input('Em quantos anos vai pagar? '))
-    meses= anos * 12
-    prestacao= valor / meses
-    limite30= sal*0.30
-    print(f'Casa de R${valor:.2f} em {anos} anos')
-    print(f'O seu limite de 30% é de R$ {limite30:.2f}')
-    print(f'A prestacao sera de R$ {prestacao:.2f}')
-
-    if prestacao<=limite30:
-        print('empréstimo pode ser CONCEDIDO')
-    else:
-        print('emprestimo NEGADO!')
-
-def ex37():
-    n=int(input('Digite um numero: '))
-    print('''Escolha uma das bases de conversão:
-        [1] Converter para binário
-        [2]Converter para octal
-        [3]Converter para hexadecimal''')
-    opcao=int(input('Sua opcao: '))
-    if opcao == 1:
-        print(f'{n} -> para binario = {bin(n)[2:]}')
-    elif opcao ==2:
-        print(f'{n} -> para octal= {oct(n)[2:]}')
-    elif opcao ==3:
-        print(f'{n} -> para hexadecimal = {hex(n)[2:]}')
-    else:
-        print(f'Opcao invalida.Tente novamente')
-          
+    valor = float(input('Valor da casa: R$ '))
+    sal = float(input('Salário: R$ '))
+    anos = int(input('Anos para pagar: '))
     
-def ex38():
-    n1=int(input('Digite o 1 numero: '))
-    n2=int(input('Digite o 2 numero: '))
-
-    if n1 > n2:
-        print(f'O primeiro valor{n1} é maior que {n2}')
-    elif n1 < n2:
-        print(f'O Segundo valor {n2} é maior que {n1}')
+    meses = anos * 12
+    prestacao = valor / meses
+    limite = sal * 0.30
+    
+    print(f'Prestação: R${prestacao:.2f} | Limite: R${limite:.2f}')
+    if prestacao <= limite:
+        print('\033[32mAPROVADO\033[m')
     else:
-        print(f'Nao existe valor maior, os 2 sao iguais, {n1}')
+        print('\033[31mNEGADO\033[m')
 
-def ex39():
-    atual=date.today().year
-
-    nasc=int(input('Ano de nascimento: '))
-    idade=atual-nasc
-    if idade == 18:
-        print('Voce tem que se alistar IMEDIATAMENTE!')
-    elif idade < 18:
-        saldo=18-idade
-        print(f'Ainda faltam {saldo} anos para o alistamento')
-        ano=atual+saldo
-        print(f'Seu alistamento será em {ano}')
+# EXERCÍCIO 37: CONVERSOR DE BASES
+def ex37():
+    n = int(input('Número inteiro: '))
+    print('[1] Binário\n[2] Octal\n[3] Hexadecimal')
+    opcao = int(input('Sua opção: '))
+    
+    if opcao == 1:
+        print(f'Binário: {bin(n)[2:]}')
+    elif opcao == 2:
+        print(f'Octal: {oct(n)[2:]}')
+    elif opcao == 3:
+        print(f'Hexadecimal: {hex(n)[2:]}')
     else:
-        saldo = idade -18
-        saldo= idade-18
-        print(f'Ja passou {saldo} anos do seu alistamento.')
+        print('Opção inválida!')
 
+# EXERCÍCIO 40: MÉDIA DO ALUNO
 def ex40():
-    n1=float(input('NOTA 1: '))
-    n2=float(input('NOTA 2: '))
-    m=(n1+n2)/2
-    if m <5.0:
-        print(f'REPROVADO! {m}')
-    elif m >=5.0 and m <=6.9:
-        print(f'RECUPERAÇÃO! {m}')
+    n1 = float(input('Nota 1: '))
+    n2 = float(input('Nota 2: '))
+    m = (n1 + n2) / 2
+    
+    print(f'Média: {m:.1f}')
+    if m < 5.0:
+        print('REPROVADO')
+    elif 5.0 <= m < 7.0:
+        print('RECUPERAÇÃO')
     else:
-        print(f'PARABÉNS! APROVADO! {m}')
+        print('APROVADO')
 
-def ex41():
-    atual=date.today().year
-    nasc=int(input('Ano de nascimento: '))
-    idade=atual-nasc
-    if idade <=9:
-        print(f'{idade} anos, Categoria MIRIM')
-    elif idade <=14:
-        print(f'{idade} anos, Categoria INFANTIL')
-    elif idade <=19:
-        print(f'{idade} anos, Categoria JUNIOR')
-    elif idade<=25:
-        print(f'{idade} anos, categoria SENIOR')
-    else:
-        print(f'{idade} anos, Categoria MASTER')
-
-def ex42():
-    a=int(input('Lado A: '))
-    b=int(input('Lado B: '))
-    c=int(input('Lado C: '))
-
-    if a < b+c and b < a+c and c <a+b:
-        print('Forma um triangulo ', end='')
-        if a == b == c: #LADOS IGUAIS
-            print('EQUILÁTERO.')
-        elif a!=b!=c!=a: #LADOS DIFERENTES
-            print('ESCALENO.')    
-        else: #2 LADOS IGUAIS, 1 DIFERENTE
-            print('ISÓSCELES.')
-
+# EXERCÍCIO 43: CALCULADORA DE IMC
 def ex43():
-    peso=float(input('Digite seu peso: '))
-    altura=float(input('Digite sua altura: '))
-    imc= peso / (altura ** 2) #altura ao quadrado
-    print(f' O IMC da pessoa é {imc:.1f}')
-
-    if imc <=18.5:
-        print(', ABAIXO DO PESO')
-    elif imc <= 25:
-        print(', PESO IDEAL')
-    elif imc <= 30:
-        print(', SOBREPESO')
-    elif imc <= 40:
-        print(' OBESIDADE')
+    peso = float(input('Peso (kg): '))
+    altura = float(input('Altura (m): '))
+    imc = peso / (altura ** 2)
+    
+    print(f'IMC: {imc:.1f} -> ', end='')
+    if imc < 18.5:
+        print('Abaixo do Peso')
+    elif imc < 25:
+        print('Peso Ideal')
+    elif imc < 30:
+        print('Sobrepeso')
+    elif imc < 40:
+        print('Obesidade')
     else:
-        print('OBESIDADE MÓRBIDA')
+        print('Obesidade Mórbida')
 
+# EXERCÍCIO 44: PAGAMENTOS
 def ex44():
-    preco=float(input('preço do produto: '))
-    print('''FORMAS DE PAGAMENTO
-    [ 1 ] à vista dinheiro/cheque
-    [ 2 ] à vista cartão
-    [ 3 ] 2x no cartão
-    [ 4 ] 3x ou mais no cartão''')
-    tipo=int(input('Escolha como vai pagar: '))
-    if tipo == 1:
-        pago = preco * 0.90
-    elif tipo == 2:
+    preco = float(input('Preço: R$'))
+    print('[1] À vista\n[2] Cartão\n[3] 2x Cartão\n[4] 3x+ Cartão')
+    op = int(input('Opção: '))
+    
+    if op == 1:
+        pago = preco * 0.9
+    elif op == 2:
         pago = preco * 0.95
-    elif tipo == 3:
+    elif op == 3:
         pago = preco
-        parcela= pago /2
-        print(f'parcelada em 2x de R${parcela:.2f} SEM JUROS')
-    elif tipo == 4:
-        pago= preco *1.2
-        totparc=int(input('Quantas parcelas? '))
-        parcela = pago / totparc
-        print(f'parcelado em {totparc}x de R${parcela:.2f} COM JUROS')
-
+        print(f'2x de R${pago/2:.2f}')
+    elif op == 4:
+        pago = preco * 1.2
+        parc = int(input('Parcelas? '))
+        print(f'{parc}x de R${pago/parc:.2f}')
     else:
-        pago=preco
-        print('opcao invalida,tente novamente!')
-    print(f'Sua compra de R${preco} vai custar {pago:.2f} no final')
+        pago = preco
+        print('Opção inválida!')
+    print(f'Total: R${pago:.2f}')
 
-
-
+# EXERCÍCIO 45: JOKENPÔ
 def ex45():
-    from random import randint
-    from time import sleep
-
     itens = ('Pedra', 'Papel', 'Tesoura')
-    pc=randint(0,2)
-
+    pc = randint(0, 2)
+    
     print('[0] Pedra\n[1] Papel\n[2] Tesoura')
-    jogador = int(input('Sua jogada: '))
-
-    if jogador not in (0, 1, 2):
-        print('JOGADA INVÁLIDA!')
-        return # Para a função aqui mesmo
-
-    print('JO... KEN... PÔ!')
-    sleep(1)
-
-    print('-='*11)
-    print(f'Computador jogou {itens[pc]}')
-    print(f'Jogador jogou {itens[jogador]}')
-    print('-='*11)
-
-    if pc == jogador:
-        res='EMPATE'
-    elif ((jogador == 0 and pc == 2) or
-        (jogador == 1 and pc == 0) or
-        (jogador == 2 and pc == 1)):
-        res='VOCE VENCEU!'
+    user = int(input('Sua jogada: '))
+    
+    if user not in (0, 1, 2):
+        print('Inválido!')
+        return
+    
+    print('JO...'), sleep(0.5)
+    print('KEN...'), sleep(0.5)
+    print('PÔ!!!')
+    
+    print(f'PC: {itens[pc]} | VOCÊ: {itens[user]}')
+    if pc == user:
+        res = 'EMPATE'
+    elif (user == 0 and pc == 2) or (user == 1 and pc == 0) or (user == 2 and pc == 1):
+        res = 'VOCÊ VENCEU'
     else:
-        res='Computador venceu!'
+        res = 'PC VENCEU'
+    print(res)
 
-    print(f'Resultado: {res}\n')
-
-        
+#CHAMADA DE FUNÇÃO
 ex45()
